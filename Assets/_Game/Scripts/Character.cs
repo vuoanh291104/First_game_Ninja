f52137aa6da12acb4c3bb066186f9b948ab228dc
Vu Oanh <nekodeath123@gmail.com>
2024-03-22 08:47:38 +0700

    vi 4

--- Assets/_Game/Scripts/Character.cs
+++ Assets/_Game/Scripts/Character.cs
@@ -7,6 +7,7 @@ public class Character : MonoBehaviour
     private float hp;
     private string currentAnimName;
     [SerializeField] private Animator anim;
+    [SerializeField] private HealthBar healthBar;
     public bool IsDead => hp<=0;  // => same return
 
     private void Start(){
@@ -15,6 +16,7 @@ public class Character : MonoBehaviour
 
     public virtual void OnInit(){
         hp = 100;
+        healthBar.OnInit(100);
     }
 
     public virtual void OnDespawn(){
@@ -36,8 +38,11 @@ public class Character : MonoBehaviour
         if(!IsDead){
             hp-=damage;
             if(IsDead){
+                hp=0;
                 OnDeath();
             }
+            healthBar.SetNewHp(hp);
+
         }
     }
     
